65a143336233c3b24b363295861455096f4ef7c3
liron revah <33122702+liron7722@users.noreply.github.com>
2020-11-13 20:31:28 +0200

    Initialize

---
+++ Scripts/Scraper/SoccerScraper.py
@@ -0,0 +1,170 @@
+#!/usr/bin/env python3
+
+import re
+from Scripts.Utility.db import DB
+from Scripts.Utility.json import save
+from Scripts.Utility.time import call_sleep
+from Scripts.Scraper.Scraper import Scraper
+from Scripts.Scraper.Sports.Soccer.Competition import Competition
+from Scripts.Utility.Exceptions import PageNotLoaded, ParseError
+
+
+class SoccerScraper(Scraper):
+    key = 'Soccer'
+    url = 'https://fbref.com/short/inc/main_nav_menu.json'  # Competitions link
+    competitions = []
+    db_client = None
+    logger = None
+
+    def __init__(self, link: str = None):
+        self.url = self.url if link is None else link
+        super().__init__(self.url, f'{self.key}.log')
+        self.db_client = DB(self.key)
+
+    def get_collection_data(self, collection_list, db):
+        data = dict()
+        for c_name in collection_list:  # Info \ Seasons
+            self.log(f'Got collection to load: {c_name}')
+            collection = self.db_client.get_collection(name=c_name, db=db)
+            # Season {Fixture \ Nationalities \ Info}, Info {URL \ Name \ Tables}
+            data[c_name] = self.db_client.get_documents_list(collection=collection)  # documents
+        return data
+
+    def load_competition(self, data):
+        values = data['Info'][0]
+        temp = Competition(key=values['Name'], url=values['Url'], logger=self.logger)
+        temp.load_db(data['Seasons'])
+        self.competitions.append(temp)
+
+    def load_db(self):
+        self.log('Cmd: load_db')
+        db_list = self.db_client.get_db_names()
+        for db_name in db_list:
+            if db_name in ['admin', 'config', 'local']:
+                continue
+            # Competition
+            self.log(f'Got db to load: {db_name}')
+            db = self.db_client.get_db(name=db_name)
+            collection_list = self.db_client.get_collections_names(db=db)
+            data = self.get_collection_data(collection_list, db=db)
+            if 'Info' in collection_list:
+                self.load_competition(data)
+
+    def set_competitions(self, values: list):
+        self.log('Cmd: set_competitions')
+        for item in values:
+            if item['target'] == 'header_comps':
+                temp = re.findall('/(.*)\"', item['html'].replace(';', '\n'))
+                for val in temp:
+                    self.add_competition(
+                        val.split('/')[-1],  # key
+                        val  # url
+                    )
+
+    def add_competition(self, key, url: str):
+        # Check competition not already added before
+        flag = True
+        for comp in self.competitions:
+            if comp.key == key:
+                flag = False
+                break
+        if flag and key not in ['2020-2021', '2022']:
+            self.log(f'Cmd: add_competitions,\tKey: {key},\tUrl: {url}')
+            self.competitions.append(
+                Competition(key=key, url=url, logger=self.logger)
+            )
+
+    def scrape_competitions(self):
+        self.log('Cmd: scrape_competitions')
+        self.log(f'Info: Going to scrape {len(self.competitions)} Competitions')
+        for comp in self.competitions:
+            try:
+                comp.scrape()  # Competition scrape
+            except PageNotLoaded or ParseError:
+                message = f'Stopped in SoccerScraper script, scrape_competitions method' \
+                          f'Competition Key: {comp.key}'
+                self.logger.exception(message) if self.logger is not None else print(message)
+
+    def scrape(self):
+        self.log('Cmd: scrape')
+        self.set_competitions(self.r_json)  # competitions from website
+        self.scrape_competitions()  # start scrape
+
+    def get_competitions(self):
+        self.log('Cmd: get_competitions')
+        return [comp.to_json() for comp in self.competitions]
+
+    def to_json(self, name: str = None, to_file: bool = False):
+        self.log(f'Cmd: to_json\t'
+                 f'Key: {self.key}\t'
+                 f'Url: {self.url}')
+        data = self.get_competitions()
+        if to_file:
+            self.log('Cmd: File saving...')
+            save(data=data, name=name)
+        else:
+            self.log('Cmd: Retrieve data')
+            return data
+
+    def season_to_db(self, season, collection):
+        data = season.to_json()
+        if self.db_client.is_exist(collection=collection, fil={"URL": season.url}):  # insert
+            self.db_client.insert_document(collection=collection, data=data)
+            self.log(f'Cmd: Insert new season document,\tUrl: {season.url}')
+        else:  # update
+            self.db_client.update_document(collection=collection,
+                                           fil={'URL': season.url},
+                                           data=data)
+            self.log(f'Cmd: Update season document,\tUrl: {season.url}')
+
+    def info_to_db(self, comp, collection):
+        self.log('Cmd: info_to_db')
+        if self.db_client.is_exist(collection=collection, fil={"URL": comp.url}):  # insert
+            data = {'URL': comp.url,
+                    'Tables': comp.get_tables()
+                    }
+            self.db_client.insert_document(collection=collection, data=data)
+            self.log('Cmd: Insert new info document')
+        else:  # update
+            self.db_client.update_document(collection=collection,
+                                           fil={"URL": comp.url},
+                                           data={'Tables': comp.get_tables()})
+            self.log('Cmd: Update info document')
+
+    def get_competition_to_db(self, comp: Competition):
+        self.log('Cmd: get_competition_to_db')
+        db = self.db_client.get_db(name=comp.key)
+
+        collection = self.db_client.get_collection(name='INFO', db=db)
+        self.info_to_db(collection=collection, comp=comp)  # INFO
+
+        collection = self.db_client.get_collection(name='Seasons', db=db)
+        for season in comp.get_seasons_as_json():  # seasons
+            self.season_to_db(season=season, collection=collection)
+
+    def to_db(self):
+        self.log('Cmd: to_db')
+        for comp in self.competitions:
+            self.get_competition_to_db(comp=comp)
+
+    def run(self):
+        self.log(f'Cmd: run\t'
+                 f'Key: {self.key}\t'
+                 f'Url: {self.url}')
+        self.load_db()
+        while True:
+            self.scrape()
+            self.to_db()
+            call_sleep(days=1, logger=self.logger)
+
+    def log(self, message: str, level: int = 10):
+        if self.logger is not None:
+            if level is not None:
+                self.logger.log(level, message)
+
+
+if __name__ == '__main__':
+    soccer_scraper = SoccerScraper()
+    soccer_scraper.load_db()
+    soccer_scraper.scrape()
+    soccer_scraper.to_db()
---
+++ Scripts/Scraper/Sports/Soccer/Competition.py
@@ -0,0 +1,156 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+from bs4 import BeautifulSoup
+from Scripts.Utility.json import save
+from Scripts.Utility.requests import connect
+from Scripts.Utility.Exceptions import PageNotLoaded
+from Scripts.Scraper.Sports.Soccer.Season import Season
+
+
+class Competition:
+    base = 'https://fbref.com/'  # main url
+    url = None
+    logger = None
+    tables = None
+    db_client = None
+    seasons: list = []
+    to_scrape: list = []
+    scraped_flag: bool = False
+
+    def __init__(self, key, url, logger=None):
+        self.key = key
+        self.url = url
+        self.add_logger(logger=logger)
+        self.log(f'Competition Initialize with Key: {key},\tUrl: {url}')
+        r_text = connect(url=self.base + self.url)
+        page_text = r_text.replace('<!--\n', '')
+        self.soup = BeautifulSoup(page_text, "lxml")
+
+    # Setters
+    def add_logger(self, logger):
+        self.logger = logger
+
+    def load_db(self, data: dict):
+        # TODO check if there is something to scrape
+        self.log(f'Cmd: load_db')
+        if data is None:
+            self.log(f'Cmd: Nothing to load')
+        else:  # Todo check if make sense
+            for season in data:
+                if len(season['To Scrape']) > 0:
+                    self.add_season(season['URL'], season['Basic Info'], len(self.seasons))
+
+    def add_season(self, url: str, info, index: int):
+        self.log(f'Cmd: add_season')
+        temp = Season(self.key, url, info)
+        temp.scrape()
+        self.seasons.append({index: temp})
+
+    # Getters
+    def get_seasons_as_json(self):
+        return [season.to_json() for season in self.seasons]
+
+    def get_tables(self):
+        return [[table.iloc[i].to_json() for i in range(len(table))] for table in self.tables]
+
+    # Utility functions
+    @staticmethod
+    def change_col_name(table):
+        # Given better col name
+        cols = table.columns
+        if type(cols) == pd.core.indexes.multi.MultiIndex:
+            table.columns = [f"{'General' if 'Unnamed' in col[0] else col[0]} - {col[1]}" for col in cols]
+        return table
+
+    @staticmethod
+    def change_nan(table):
+        return table.where(pd.notnull(table), None)  # Nan to None
+
+    def parse_general_info(self):
+        self.log(f'Cmd: parse_general_info')
+        temp = []
+        dfs = pd.read_html(str(self.soup))
+        for table in dfs:
+            table = self.change_col_name(table)
+            table = self.change_nan(table)
+            temp.append(table)
+        self.tables = temp
+
+    def get_history_link(self):
+        self.log(f'Cmd: get_history_link')
+        temp = self.soup.find_all('ul', {'class': "hoversmooth"})[1]  # navbar html
+        # navbar index (history) url
+        index_url = temp.find('li', {'class': "index"}).find('a').get('href').removeprefix('/')
+        return self.base + index_url
+
+    # Scrape functions
+    def scrape_seasons(self, soup: BeautifulSoup, scrape_list: list):
+        self.log(f'Cmd: scrape_seasons')
+        df = pd.read_html(str(soup))[0]  # get history full table
+        urls = soup.find('tbody').find_all('tr')  # get links of all seasons
+        scrape_list = [j for j in range(len(urls))] if len(scrape_list) == 0 else scrape_list
+        self.log(f'Info: Going to scrape {len(scrape_list)} Seasons')
+        for i in scrape_list:
+            info = df.iloc[i]  # general information
+            if urls[i].contents[0].find('a') is None:  # this row don't have a url
+                self.log(f'Skipped row {i} while trying to parse season table')
+                continue
+            else:
+                url = urls[i].contents[0].find('a').get('href')  # Competition url
+            try:
+                self.add_season(url=url, info=info, index=i)
+                self.log(f'Season {i}/{len(scrape_list)} successfully scrape at Url: {url}', level=20)
+            except PageNotLoaded:
+                self.log(f'Error accord,\tSeason {i}/{len(scrape_list)} failed scrape at Url: {url}')
+                if i not in self.to_scrape:  # avoiding duplicate
+                    self.to_scrape.append(i)
+        self.scraped_flag = True
+
+    def scrape(self):
+        self.log(f'Cmd: scrape\t'
+                 f'Key: {self.key}\t'
+                 f'Url: {self.url}')
+        link = self.get_history_link()
+        # check if current position is the history url
+        soup = BeautifulSoup(connect(link), "lxml")
+        self.scrape_seasons(soup, self.to_scrape)  # Starting call scrape for each season
+        self.is_scraped()  # Going throw all season check who didn't scrape
+
+    # Results functions
+    def is_scraped(self):
+        self.log(f'Cmd: is_scraped')
+        for item in self.seasons:
+            for index, season in item.items():
+                if season.is_scraped() is False:
+                    self.log(f'{season.key} scrape status: Incomplete')
+                    self.scraped_flag = False
+                    if index not in self.to_scrape:
+                        self.log(f'Index {index} added to scrape list')
+                        if index not in self.to_scrape:
+                            self.to_scrape.append(index)
+        self.log(f'Competition scrape status: {"In" * (not self.scraped_flag)}Complete\t'
+                 f'Key: {self.key}')
+        return self.scraped_flag
+
+    def to_json(self, name: str = None, file: bool = False):
+        self.log(f'Cmd: to_json\t'
+                 f'Key: {self.key}\t'
+                 f'Url: {self.url}')
+        data = {"Info": {'URL': self.url,
+                         'Name': self.key,
+                         'Tables': self.get_tables()
+                         },
+                'Seasons': self.get_seasons_as_json()
+                }
+        if file:
+            self.log('Cmd: File saving...')
+            save(data=data, name=name)
+        else:
+            self.log('Cmd: Retrieve data')
+            return data
+
+    def log(self, message: str, level: int = 10):
+        if self.logger is not None:
+            if level is not None:
+                self.logger.log(level, message)
