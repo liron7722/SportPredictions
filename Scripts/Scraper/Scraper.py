57e0f93272449cc5f22beee9d20dc712c56672de
liron revah <33122702+liron7722@users.noreply.github.com>
2020-11-30 02:16:01 +0200

    better code

--- Scripts/Scraper/Scraper.py
+++ Scripts/Scraper/Scraper.py
@@ -6,7 +6,12 @@ from Scripts.Utility.requests import connect
 
 
 class Scraper:
-    def __init__(self, link: str, name: str = "NoName.log"):
-        self.logger = Logger(f'{name}.log').get_logger()
+    logger = None
+
+    def __init__(self, link: str, name: str = None):
+        self.add_logger(name=name) if name is str else self.add_logger()
         self.db_client = DB(self.logger)
         self.r_json = connect(url=link, return_text=False, return_json=True)
+
+    def add_logger(self, name: str = "NoName.log"):
+        self.logger = Logger(f'{name}.log').get_logger()
