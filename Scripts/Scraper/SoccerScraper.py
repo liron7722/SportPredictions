919cf090b138bb647da53e5a7f19414b62fdcb45
liron revah <33122702+liron7722@users.noreply.github.com>
2020-12-09 15:14:42 +0200

    changed scraped flow

--- Scripts/Scraper/SoccerScraper.py
+++ Scripts/Scraper/SoccerScraper.py
@@ -20,37 +20,30 @@ class SoccerScraper(Scraper):
         super().__init__(url=self.url, name=self.key)
 
     # Scrape
-    def set_competitions(self, values: list):
-        self.log('Cmd: set_competitions')
-        for item in values:
-            if item['target'] == 'header_comps':
-                temp = re.findall('/(.*)\"', item['html'].replace(';', '\n'))
-                for val in temp:
-                    self.add_competition(key=val.split('/')[-1], url=val)
+    def scrape_competitions(self, comp: Competition):
+        self.log('Cmd: scrape_competitions')
+        try:
+            time_wrapper(func=comp.run, logger=self.logger)()  # Competition scrape
+        except PageNotLoaded or ParseError:
+            message = f'Error stopped in SoccerScraper script, scrape_competitions method\t' \
+                      f'Competition Key: {comp.key}'
+            self.logger.exception(message) if self.logger is not None else print(message)
 
     def add_competition(self, key, url: str):
         flag = not (key in [comp.key for comp in self.competitions])  # Check competition not already added before
         if flag and key not in ['2020-2021', '2022']:  # Current and future season, will be scraped in other urls
             self.log(f'Cmd: add_competitions,\tKey: {key},\tUrl: {url}')
-            self.competitions.append(
-                Competition(key=key, url=url, logger=self.logger, db=self.db_client)
-            )
-
-    def scrape_competitions(self):
-        self.log('Cmd: scrape_competitions')
-        self.log(f'Info: Going to scrape {len(self.competitions)} Competitions')
-        for comp in self.competitions:
-            try:
-                time_wrapper(func=comp.run, logger=self.logger)()  # Competition scrape
-            except PageNotLoaded or ParseError:
-                message = f'Error stopped in SoccerScraper script, scrape_competitions method\t' \
-                          f'Competition Key: {comp.key}'
-                self.logger.exception(message) if self.logger is not None else print(message)
+            temp = Competition(key=key, url=url, logger=self.logger, db=self.db_client)
+            time_wrapper(func=self.scrape_competitions, logger=self.logger)(comp=temp)  # start scrape
+            self.competitions.append(temp)
 
     def scrape(self):
         self.log('Cmd: scrape')
-        self.set_competitions(self.r_json)  # competitions from website
-        time_wrapper(func=self.scrape_competitions, logger=self.logger)()  # start scrape
+        for item in self.r_json:
+            if item['target'] == 'header_comps':
+                temp = re.findall('/(.*)\"', item['html'].replace(';', '\n'))
+                for val in temp:
+                    self.add_competition(key=val.split('/')[-1], url=val)
 
     # Main
     def run(self, loop: bool = True):
