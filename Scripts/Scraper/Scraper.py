e4e7f9ee5815179e86c2666ae2cf696c3aaf20d6
liron revah <33122702+liron7722@users.noreply.github.com>
2020-12-09 15:14:20 +0200

    disable db upload because of the documents size limit

--- Scripts/Scraper/Scraper.py
+++ Scripts/Scraper/Scraper.py
@@ -13,7 +13,7 @@ class Scraper:
 
     def __init__(self, url: str, name: str = None, to_db: bool = True):
         self.add_logger(name=name) if name is not None else self.add_logger()
-        self.db_client = DB(key=name, logger=self.logger) if to_db else None
+        # self.db_client = DB(key=name, logger=self.logger) if to_db else None
         self.r_json = connect(url=url, return_text=False, return_json=True)
         self.ENV = environ.get(f'{name}_SCRAPER_ENV')
 
