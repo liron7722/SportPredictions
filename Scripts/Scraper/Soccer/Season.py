b14e8c90a6cffde36bb406eb0ea6c61ea7df3822
liron revah <33122702+liron7722@users.noreply.github.com>
2021-01-06 19:19:58 +0200

    readable change

--- Scripts/Scraper/Soccer/Season.py
+++ Scripts/Scraper/Soccer/Season.py
@@ -170,7 +170,9 @@ class Season(Basic):
                 self.logger.exception(message) if self.logger is not None else print(message)
                 continue
             try:
-                if html_urls[i].contents[-1] == 'Match Cancelled' or temp.text == 'Head-to-Head' or len(temp.attrs) != 2:
+                if html_urls[i].contents[-1] == 'Match Cancelled' \
+                        or temp.text == 'Head-to-Head'\
+                        or len(temp.attrs) != 2:
                     self.to_scrape.append(i)  # fixture don't have match link - yet to happen or postpone or cancelled
                 elif temp.text == 'Match Report':
                     url = self.extract_url(temp.find('a').get('href'))  # Fixture url
