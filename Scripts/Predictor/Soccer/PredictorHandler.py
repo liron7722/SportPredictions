6f92ebd8eb1026301ed568bdde3d98d3c1d32c69
liron revah <33122702+liron7722@users.noreply.github.com>
2021-01-31 22:43:13 +0200

    small change and get models info function

--- Scripts/Predictor/Soccer/PredictorHandler.py
+++ Scripts/Predictor/Soccer/PredictorHandler.py
@@ -1,3 +1,4 @@
+import gc
 from Scripts.Utility.db import DB
 from Scripts.Utility.logger import Logger
 from Scripts.Predictor.Soccer.RFR import RFR
@@ -9,6 +10,7 @@ class PredictorHandler:
 
     def __init__(self):
         self.name = 'Predictor_Handler'
+        gc.enable()
         self.logger = self.get_logger()
         self.db_client = self.get_db()
         self.load()
@@ -37,13 +39,42 @@ class PredictorHandler:
             result[key] = predictor.predict(comp_key=comp_key, data=data)
         return result
 
-    def info(self):
-        result = dict()
+    def get_models_info(self):
+        models = dict()
         for key, predictor in self.predictors.items():
-            result[key] = dict()
+            models[key] = dict()
             for comp_key in predictor.get_competition():
-                result[key][comp_key] = list(predictor.get_prediction_key(comp_key))
-        return result
+                models[key][comp_key] = list(predictor.get_prediction_key(comp_key))
+        return models
+
+    def get_comps_info(self):
+        comps_info = dict()
+        db = self.db_client.get_db('Data-Handling')
+        competition_names = self.db_client.get_collections_names(db=db)
+
+        # Init
+        for col in ['Teams', 'Managers', 'Referees']:
+            competition_names.remove(col)
+        for comp in competition_names:
+            comps_info[comp] = {'Teams': set(), 'Managers': set(), 'Referees': set()}
+
+        # Append
+        for col in ['Teams', 'Managers', 'Referees']:
+            collection = self.db_client.get_collection(col, db)
+            documents = self.db_client.get_documents_list(collection, fil={'Season': 'All Time'})
+            for item in documents:
+                name = item['Name']
+                comp = item['Competition']
+                comps_info[comp][col].add(name)
+
+        for comp in comps_info.keys():
+            for col in comps_info[comp].keys():
+                comps_info[comp][col] = list(comps_info[comp][col])
+        return comps_info
+
+    def info(self):
+        return {'Competitions': self.get_comps_info(),
+                'Models': self.get_models_info()}
 
     def run(self):
         for key, predictor in self.predictors.items():
