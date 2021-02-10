1d7418fc3a53dd5197735a40f44e90bb1206ea55
liron revah <33122702+liron7722@users.noreply.github.com>
2021-02-10 16:54:23 +0200

    better logger name

--- Scripts/Scraper/Scraper.py
+++ Scripts/Scraper/Scraper.py
@@ -15,10 +15,10 @@ class Scraper:
         self.add_logger(name=name) if name is not None else self.add_logger()
         self.db_client = DB(key=name, logger=self.logger) if to_db else None
         self.r_json = connect(url=url, return_text=False, return_json=True)
-        self.ENV = environ.get(f'{name}_SCRAPER_ENV')
+        self.ENV = environ.get(f'ENV')
 
     def add_logger(self, name: str = "NoName"):
-        self.logger = Logger(f'{name}.log').get_logger()
+        self.logger = Logger(f'{name}_Scraper.log').get_logger()
 
     # Logger
     def log(self, message: str, level: int = 10):
